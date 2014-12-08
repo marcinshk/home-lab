8403a87fab84c9db7e1f30cccd6943de4dc7ea9e
marcinshk <marcinshk@gmail.com>
2014-12-08 22:14:34 +0100

    Update vagrant-provision.sh

--- vagrant-provision.sh
+++ vagrant-provision.sh
@@ -1,6 +1,35 @@
 #!/bin/bash
 
-echo -e "#########################\n  Welcome!\n#########################" >> /etc/motd
+# Create a custom MOTD
+MOTD_TEXT=`cat <<__MOTD__
+#########################
+
+ WELCOME!
 
 
+ This system has been customized:
+ - installed Python 'pip' and 'virtualenv'
 
+
+ ENJOY!
+
+#########################
+
+__MOTD__
+`
+
+echo "${MOTD_TEXT}" > /etc/motd
+
+#--
+
+# Install some Python addons (not sure if this is required)
+yum -y install python-py.noarch
+
+# Install Python 'pip' utility
+wget --no-check-certificate -O /vagrant/get-pip.py https://bootstrap.pypa.io/get-pip.py
+chmod +x /vagrant/get-pip.py
+
+# Install Python 'virtualenv'
+pip install virtualenv
+
+# EOF
